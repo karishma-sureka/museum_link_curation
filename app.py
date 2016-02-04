f3b3a445048df9ab38a3351c50227d400fa35007
Nilay Chheda <mit2nil@gmail.com>
2016-02-04 10:59:31 -0800

    Added debug toolbar for flask

--- app.py
+++ app.py
@@ -2,14 +2,17 @@
 import os, sys
 from flask import Flask, render_template
 from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
+from flask_debugtoolbar import DebugToolbarExtension
 
 webapp = Flask(__name__)
+webapp.debug = True
 webapp.config['MONGODB_DB'] = 'Link_Curation_Interface'
 webapp.config['MONGODB_HOST'] = '127.0.0.1'
 webapp.config["SECRET_KEY"] = "KeepThisS3cr3t"
 webapp.config['DEBUG_TB_PANELS'] = ['flask.ext.mongoengine.panels.MongoDebugPanel']
 db = MongoEngine(webapp)
 webapp.session_interface = MongoEngineSessionInterface(db)
+toolbar = DebugToolbarExtension(webapp)
 
 @webapp.route("/")
 def main():
