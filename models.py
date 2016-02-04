247705ffa0345438d58aeb23f4db12adc5ea49a9
Nilay Chheda <mit2nil@gmail.com>
2016-02-04 10:33:24 -0800

    Updated compile errors in models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class Artist(db.Document):
     person = db.ReferenceField('Person',requied=True)
     references = db.ListField(db.ReferenceField('Reference'))
     
-class Status(db.EmbeddedDocument)
+class Status(db.EmbeddedDocument):
     # 1 - Not Started, 2 - In Progress, 3 - Completed, 4 - Disagreement
     value = db.IntField(max_lenght=4,required=True)
     description = db.StringField(max_length=255, required=True)
@@ -18,7 +18,7 @@ class Curator(db.Document):
     name = db.StringField(max_length=255,required=True)
     rating = db.IntField(required = True)
     questionsSeen = db.ListField(db.ReferenceField('Question'))
-    tags = db.ListField(ReferenceField('Tag'))
+    tags = db.ListField(db.ReferenceField('Tag'))
     
 class Answer(db.Document):
     # 1 - Yes, 2 - No, 3 - Not Sure
@@ -29,9 +29,9 @@ class Answer(db.Document):
 class Question(db.Document):
     status = db.EmbeddedDocumentField('Status', required=True)
     timestamp = db.DateTimeField(default=datetime.datetime.now, required=True)
-    tags = db.ListField(ReferenceField('Tag'))
+    tags = db.ListField(db.ReferenceField('Tag'))
     curationSubject = db.ReferenceField('Artist',required=True) #Here Museum artist
-    decision = db.ListField(ReferenceField('Answer'))
+    decision = db.ListField(db.ReferenceField('Answer'))
     
     def get_status(self):
         return self.status
