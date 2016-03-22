996bc4ef4f2b5be36907c6ee4f423d04cff4266c
mit2nil <mit2nil@gmail.com>
2016-03-22 16:33:51 -0700

    Cleaned the backend and enhanced core API support

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-# Set the path
-import os, sys
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 from pymongo import MongoClient
 from dbMgr import *
-
-#from flask.ext.mongoengine import MongoEngine
-#from flask_restful_swagger import swagger
-#from restfulInterface import *
 
 webapp = Flask(__name__)
 api = Api(webapp)
 dbClient = MongoClient('localhost', 27017)
 dbName = "TestDb1"
 
+# Uncommment below lines only once
 #cleanDatabase(dbClient,dbName)
 #createDatabase(dbClient,dbName)
 #printDatabase(dbClient,dbName)
@@ -23,16 +18,27 @@ qid = 0
 aid = 0
 comment = "Default comment"
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
 class answers(Resource):
     def put(self):
+        # Input validation
+        if not 'comment' in request.form:
+            a_comment = "No Comment Provided"
+        else:
+            a_comment = request.form['comment']
+            
+        if not 'value' in request.form:
+            return {'status': 400, 'message': 'value not provided with ther request'}
+        if not 'qid' in request.form:
+            return {'status': 400, 'message': 'qid not provided with ther request'}
+        a_value = request.form['value']
         qid = request.form['qid']
-        aid = request.form['aid']
-        comment = request.form['comment']
-        return {'comment':comment,'qid':qid, 'aid':aid}
+        
+        answer = {"value":a_value,
+          "comment":a_comment,
+          "author":"dakldsjkaldjoi3uoiuje3kl2knkjn" # Random string, Get object ID of curator
+         }
+        submitAnswer(dbClient,dbName,qid,answer)
+        return {'qid':qid,'author':answer['author'],'value':answer['value'],'comment':answer['comment']}
     
     def get(self):
         return {'comment':comment,'qid':qid, 'aid':aid}
@@ -41,50 +47,25 @@ class questions(Resource):
     def get(self):
         question = getQuestion(dbClient,dbName,"nilayvac@usc.edu")
         return {'qid': str(question['_id']),"uri1":question['uri1'],"uri2":question['uri2']}
+        
+        #from flask import jsonify # jsonify usage
+        #return jsonify({'answerSubmitted':a.value,'comment':a.comment,'Author':a.author.name}), 201
 
-api.add_resource(HelloWorld, '/v1/')
+class homepage(Resource):
+    def get(self):
+        return render_template('index.html')
+
+api.add_resource(homepage, '/v1/')
 api.add_resource(answers, '/v1/answer')
 api.add_resource(questions, '/v1/question')
 
 @webapp.route('/')
 def indexPage():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        print request.form['username']
-        return redirect(url_for('index'))
+    # if request.method == 'POST':
+        # session['username'] = request.form['username']
+        # print request.form['username']
+        # return redirect(url_for('index'))
     return render_template('index.html')
-
-# @webapp.route('/get-q', methods=['POST'])
-# def rest_get_question():
-    # # Bad input conditions
-    # if not request.json:
-        # abort(400)
-    # if not 'uid' in request.json:
-        # abort(400)
-        
-    # return jsonify({'qid':q.qid,'uri1':q.uri1,'uri2':q.uri2}), 201
-   
-# @webapp.route('/submit-a', methods=['POST'])
-# def rest_submit_answer():
-    # # Bad input conditions
-    # if not request.json:
-        # abort(400)
-    # if not 'qid' in request.json:
-        # abort(400)
-    # if not 'uid' in request.json: # link to Answer.curator
-        # abort(400)
-    # if not 'answer' in request.json: # Answer.value (# 1 - Yes, 2 - No, 3 - Not Sure)
-        # abort(400)
-       
-    # if not 'comment' in request.json:
-        # #ans = Answer(value=request.json['answer'],comment="",author=Curator.objects(uid=uid))
-        # ans = Answer(value=request.json['answer'],comment="",author=Curator(uid="nilayvac@usc.edu",name="Nilay Chheda",rating=5))
-    # else:
-        # #ans = Answer(value=request.json['answer'],comment=request.json['comment'],author=Curator.objects(uid=uid))
-        # ans = Answer(value=request.json['answer'],comment=request.json['comment'],author=Curator(uid="nilayvac@usc.edu",name="Nilay Chheda",rating=5))
-        
-    # a = submit_answer(request.json['qid'],ans)
-    # return jsonify({'answerSubmitted':a.value,'comment':a.comment,'Author':a.author.name}), 201
    
 @webapp.route('/user/<username>')
 def show_user_profile(username):
--- dbMgr.py
+++ dbMgr.py
@@ -2,6 +2,10 @@ import datetime
 import random
 from pymongo import MongoClient
 
+confedenceLevel = 2
+contrastLevel = 2
+
+# Print the database just to check current data
 def printDatabase(dbC,dname):
     print "Printing ",dname," if it exists"
     for cname in dbC[dname].collection_names(include_system_collections=False):
@@ -9,6 +13,17 @@ def printDatabase(dbC,dname):
         for val in dbC[dname][cname].find():
             print " Document ", val
 
+# Removes all the data from all tables of given database
+def cleanDatabase(dbC,dname):
+    print "Dropping database",dname,"if it exists"
+    for cname in dbC[dname].collection_names(include_system_collections=False):
+        print "Dropping collection (aka database)",cname
+        for val in dbC[dname][cname].find():
+            print val
+        dbC[dname][cname].delete_many({})
+        dbC[dname][cname].drop()
+        
+# Create database with default values for curation
 def createDatabase(dbC,dname):
     populateTags(dbC,dname)
     populateCurators(dbC,dname)
@@ -20,6 +35,9 @@ def createDatabase(dbC,dname):
     
     #populateRefURI(dbC,dname)
     
+#Tag
+    #tagname, string 
+# Populate database with default tags
 def populateTags(dbC,dname):
     t = dbC[dname]["tag"]
     te = {"tagname":"randomtag1"}
@@ -35,6 +53,14 @@ def populateTags(dbC,dname):
     te = {"tagname":"randomtag6"}
     t.insert_one(te)
     
+#Curator
+    #uid, String - userID
+    #name, String
+    #rating, Integer
+    #questionsSeen, List of object IDs from Question
+    #tags, list of object IDs from Tags
+    
+# Populate database with default curators
 def populateCurators(dbC,dname):
     c = dbC[dname]["curator"]
     ce = {"uid":"nilayvac@usc.edu",
@@ -43,88 +69,100 @@ def populateCurators(dbC,dname):
                   dbC[dname]["tag"].find_one({'tagname':"randomtag4"})],
           "rating":5}
     c.insert_one(ce)
+    ce = {"uid":"ksureka@usc.edu",
+          "name":"Karishma Sureka",
+          "tags":[dbC[dname]["tag"].find_one({'tagname':"randomtag2"}),
+                  dbC[dname]["tag"].find_one({'tagname':"randomtag3"})],
+          "rating":5}
+    c.insert_one(ce)
 
+# Add the new curator from the client interface
+def addCurator(dbC, dname, ce):
+    c = dbC[dname]["curator"]
+    c.insert_one(ce)
+    
+# Question
+    #status, integer: 1 - Not Started, 2 - In Progress, 3 - Completed, 4 - Disagreement
+    #lastSeen, datetime field to select question based on time it was asked to previous curator
+    #tags, list of object IDs from Tags
+    #uri1, for now, just a URI related to a specific artist
+    #uri2, for now, just another URI related to same specific artist
+    #decision, list of object IDs from Answer
+    
+# Populate default set of questions
 def populateQuestions(dbC,dname):
     q = dbC[dname]["question"]
-    default_status = {"value":1,"desc":"Not Started"}
     
-    qe = {"status":default_status,
-          "timestamp": datetime.datetime.utcnow(),
+    qe = {"status":1,
+          "lastSeen": datetime.datetime.utcnow(),
           "tags":[dbC[dname]["tag"].find_one({'tagname':"randomtag1"}),
                   dbC[dname]["tag"].find_one({'tagname':"randomtag4"})],
            "uri1":"http://vocab.getty.edu/ulan/500028092",
-           "uri2":"http://edan.si.edu/saam/id/person-institution/1681"
-           #"decision": Should be updated in submtit answer
+           "uri2":"http://edan.si.edu/saam/id/person-institution/1681",
+           "decision": [] #Should be updated in submit answer
          }
     q.insert_one(qe)
     
-    qe = {"status":default_status,
-          "timestamp": datetime.datetime.utcnow(),
+    qe = {"status":1,
+          "lastSeen": datetime.datetime.utcnow(),
           "tags":[dbC[dname]["tag"].find_one({'tagname':"randomtag2"}),
                   dbC[dname]["tag"].find_one({'tagname':"randomtag3"})],
            "uri1":"http://vocab.getty.edu/ulan/500020062",
-           "uri2":"http://edan.si.edu/saam/id/person-institution/26558"
-           #"decision": Should be updated in submtit answer
+           "uri2":"http://edan.si.edu/saam/id/person-institution/26558",
+           "decision": [] #Should be updated in submit answer
          }
     q.insert_one(qe)
-    
-def cleanDatabase(dbC,dname):
-    print "Dropping database",dname,"if it exists"
-    for cname in dbC[dname].collection_names(include_system_collections=False):
-        print "Dropping collection (aka database)",cname
-        for val in dbC[dname][cname].find():
-            print val
-        dbC[dname][cname].delete_many({})
-        dbC[dname][cname].drop()
-
-def populateDatabase(dbC,dname):
-    print "Creating database"
-
+        
+# Retrieve set of questions from database based on tags, lastseen, unanswered vs in progress
 def getQuestion(dbC,dname,uid):
     #user = dbC[dname]["curator"].find_one({'uid':uid})
     #question = dbC[dname]["question"].find_one({'tags':user.tags})
-    default_status = {"value":1,"desc":"Not Started"}
-    q = dbC[dname]["question"].find_one({"status":default_status})
-    return q
-    
     #q = dbC[dname]["question"].find({"status":default_status})
     #print q
     #return random.choice(q)
-    
-# Sample Schema
+    q = dbC[dname]["question"].find_one({"status":1})
+    return q
 
-# class Artist(db.Document):
-    # person = db.StringField(max_length=255)
-    # reference = db.StringField(max_length=255)
-    # #person = db.ReferenceField('Person',requied=True)
-    # #references = db.ListField(db.ReferenceField('Reference'))
+#Answer
+    #value, Integer value - 1 - Yes, 2 - No, 3 - Not Sure
+    #comment, String optional 
+    #author, String - uid of curator 
+def submitAnswer(dbC, dname, qid, answer):
+    # Add answer to database
+    a = dbC[dname]["answer"]
+    aid = a.insert_one(answer).inserted_id
     
-# class Status(db.EmbeddedDocument):
-    # # 1 - Not Started, 2 - In Progress, 3 - Completed, 4 - Disagreement
-    # value = db.IntField(max_length=4,required=True)
-    # desc = db.StringField(max_length=255, required=True)
-
-# class Tag(db.Document):
-    # tagname = db.StringField(max_lenth=255,required=True)
-
-# class Curator(db.Document):
-    # uid = db.StringField(max_length=255,required=True) # Unique uid (primary key)
-    # name = db.StringField(max_length=255)
-    # rating = db.IntField(required = True)
-    # questionsSeen = db.ListField(db.ReferenceField('Question'))
-    # tags = db.ListField(db.ReferenceField('Tag'))
+    # from qid retrieve question 
+    q = dbC[dname]["question"].find_one({'_id':qid})
     
-# class Answer(db.Document):
-    # # 1 - Yes, 2 - No, 3 - Not Sure
-    # value = db.IntField(max_lenght=3,required=True)
-    # comment = db.StringField(max_length=255)
-    # author = db.ReferenceField('Curator')
+    if q != None:
+        # update descision with answer object id
+        q['decision'] = q['decision']+[aid]
+        # retrieve all answers
+        ans = []
+        noYes = 0
+        noNo = 0
+        noNotSure = 0
+        for aid in q['decision']:
+            a = dbC[dname]["answer"].find_one({'_id':aid})
+            ans = ans+[a]        
+            if ans['value'] == 1:
+                noYes = noYes + 1
+            elif ans['answer'] == 2:
+                noNo = noNo + 1
+            elif ans['value'] == 3:
+                noNotSure = noNotSure + 1
+                
+        # Update status of the question based on different answers
+        if noYes !=0 and noNo != 0:
+            if noYes - noNo > confedenceLevel:
+                if noNo >= contrastLevel:
+                    q['status'] = 3 # Update to in Disagreement 
+                else:
+                    q['status'] = 4 # Update to in Completed 
+            else:
+                q['status'] = 2 # Update to in Progress 
     
-# class Question(db.Document):
-    # qid = db.IntField(required=True)    # Unique qid (primary key)
-    # status = db.EmbeddedDocumentField('Status', required=True)
-    # timestamp = db.DateTimeField(default=datetime.datetime.now, required=True)
-    # tags = db.ListField(db.ReferenceField('Tag'))
-    # uri1 = db.ReferenceField('Artist',required=True) #Here Museum artist
-    # uri2 = db.ReferenceField('Artist',required=True) #Here Museum artist
-    # decision = db.ListField(db.ReferenceField('Answer'))
+        print q # Print new values of question before updating database
+        #update database entry of question with new status and decision
+        dbC[dname]["question"].find_one_and_update({'_id': qid},{'$set': {'status':q['status'],'decision':q['decision']}}) 
