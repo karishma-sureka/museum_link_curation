a11824ef5288b1e7a221f154e67356f208cc2aff
karishma-sureka <karishma2812@gmail.com>
2016-08-30 17:15:00 -0700

    fixing ui reference for rest endpoint

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 from flask import Flask, render_template, request, url_for, jsonify, redirect, flash, jsonify
 devmode = True
+if devmode:
+    server = "http://localhost:5000/"
+else:
+    server = "http://52.37.251.245"
 app = Flask(__name__)
